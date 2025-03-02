602b691b5910cb293765e5e0d873d525bb5377ee
moxforever <moxforever10@gmail.com>
2025-03-03 01:00:23 +0600

    finish editing template

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
+#!/usr/bin/sh
 
-python -m poetry run \
-    alembic upgrade head
-python -m poetry run \
-    uvicorn main:app --bind=0.0.0.0:$PORT --workers=$WORKERS
+alembic upgrade head
+uvicorn main:app --host=0.0.0.0 --port=$PORT --workers=$WORKERS
