b683f6770424f84b6c493e787beb189a5e60f1c0
moxforever <moxforever10@gmail.com>
2025-03-02 23:45:48 +0600

    edit init project

--- init_project.py
+++ init_project.py
@@ -1,16 +1,29 @@
 import os
+import subprocess
+
+
+def read_file_lines(file_path: str) -> list[str]:
+    with open(file_path, "r") as f:
+        return f.read().split("\n")
+
+
+def write_file_lines(file_path: str, lines: list[str]):
+    with open(file_path, "w") as f:
+        f.write("\n".join(lines))
 
 
 def init_project():
     project_name = input("Enter project name: ")
     project_description = input("Enter project description: ")
 
-    project_author = os.system("git config user.name")
-    project_email = os.system("git config user.email")
+    project_author = (
+        subprocess.check_output(["git", "config", "user.name"]).strip().decode("utf-8")
+    )
+    project_email = (
+        subprocess.check_output(["git", "config", "user.email"]).strip().decode("utf-8")
+    )
 
-    with open("pyproject.toml", "r") as f:
-        pyproject = f.read().split("\n")
-
+    pyproject = read_file_lines("pyproject.toml")
     for i, line in enumerate(pyproject):
         if line.startswith("name = "):
             pyproject[i] = f'name = "{project_name}"'
@@ -19,8 +32,7 @@ def init_project():
         elif line.startswith("authors = "):
             pyproject[i] = f'authors = ["{project_author} <{project_email}>"]'
 
-    with open("pyproject.toml", "w") as f:
-        f.write("\n".join(pyproject))
+    write_file_lines("pyproject.toml", pyproject)
 
     os.rename("./aiogram_template", project_name)
 
@@ -28,17 +40,14 @@ def init_project():
         for file in files:
             if not file.endswith(".py"):
                 continue
+            else:
+                filepath = os.path.join(root, file)
 
-            filepath = os.path.join(root, file)
-            with open(filepath, "r") as f:
-                content = f.readlines()
-
+            content = read_file_lines(filepath)
             for i, l in enumerate(content):
                 if l.startswith("from"):
                     content[i] = l.replace("aiogram_template", project_name)
-
-            with open(filepath, "w") as f:
-                f.writelines(content)
+            write_file_lines(filepath, content)
 
     os.system("poetry install --no-root")
 
